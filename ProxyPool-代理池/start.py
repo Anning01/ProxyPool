f24cf33bf9173e47917231714042ed76323bb363
Anning01 <39256752+Anning01@users.noreply.github.com>
2018-08-16 21:59:21 +0800

    Add files via upload

---
+++ ProxyPool-代理池/start.py
@@ -0,0 +1,12 @@
+"""
+-*- coding: utf-8 -*-
+@Time    : 2018/8/11 9:59
+@Author  : Anning
+@File    : start.py
+@Software: PyCharm
+@Contact : 864399407@qq.com
+@Bolgs   : https://blog.csdn.net/Python_anning
+"""
+from scrapy.cmdline import execute
+
+execute(['spider_66ip', 'spider_89ip', 'spider_conderbusy', 'spider_data5u', 'spider_xicidaili', 'spider_kuaidaili'])
